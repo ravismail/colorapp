b6e15f3ae893ea1cb75a41f4741767f69b5f68ce
ravi-harmony <ravikanth@harmony-hoa.com>
2025-10-17 11:56:31 -0400

    updated to red

--- app/demo/src/main/java/com/example/demo/DemoApplication.java
+++ app/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class DemoApplication {
 
-    @Value("${COLOR:yello}") // default color = blue
+    @Value("${COLOR:red}") // default color = blue
     private String color;
 
     @GetMapping("/")
